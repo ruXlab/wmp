e9ee96009ffd0d1861589c3aabcfdaa39a7d6987
ruX <git@rux.vc>
2020-06-18 16:08:04 +0100

    Compute age and years retirement

--- src/main/kotlin/vc/rux/codetest/wmp/models/Customer.kt
+++ src/main/kotlin/vc/rux/codetest/wmp/models/Customer.kt
@@ -1,6 +1,7 @@
 package vc.rux.codetest.wmp.models
 
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit.YEARS
 
 data class Customer(
     val customerId: CustomerId,
@@ -8,4 +9,10 @@ data class Customer(
     val dateOfBirth: LocalDate,
     val riskLevel: Int,
     val retirementAge: Int
-)
+) {
+    fun age(toDate: LocalDate = LocalDate.now()): Long
+        = YEARS.between(dateOfBirth, toDate)
+
+    fun yearsToRetire(today: LocalDate): Long
+        = retirementAge - age(today)
+}
---
+++ src/test/kotlin/vc/rux/codetest/wmp/models/CustomerTest.kt
@@ -0,0 +1,48 @@
+package vc.rux.codetest.wmp.models
+
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import org.assertj.core.api.Assertions.*
+
+internal class CustomerTest {
+    @Test
+    fun `compute age`() {
+        // given
+        val today = LocalDate.of(2020, 6, 18)
+        val customer = Customer(
+            customerId = CustomerId(1),
+            email = "bob@bob.com",
+            dateOfBirth = LocalDate.of(1961, 4, 29),
+            riskLevel = 3,
+            retirementAge = 65
+        )
+
+        // when
+        val age = customer.age(today)
+        
+        // then
+        assertThat(age)
+            .isEqualTo(59)
+    }
+
+    @Test
+    fun `compute years to retire`() {
+        // given
+        val today = LocalDate.of(2020, 6, 18)
+        val customer = Customer(
+            customerId = CustomerId(1),
+            email = "bob@bob.com",
+            dateOfBirth = LocalDate.of(1961, 4, 29),
+            riskLevel = 3,
+            retirementAge = 65
+        )
+
+        // when
+        val yearsToRetire = customer.yearsToRetire(today)
+
+        // then
+        assertThat(yearsToRetire)
+            .isEqualTo(6)
+    }
+
+}
